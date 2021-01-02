1c3f8db617eb6948ec2c680cc258ebc8e59203e9
Heydad Helfer <46278383+Heydad-Helfer@users.noreply.github.com>
2021-01-02 11:33:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import http.client
 
-conn = http.client.HTTPSConnection("quotes-end-of-day.p.rapidapi.com")
+conn = http.client.HTTPSConnection("securities-end-of-day-trading-data.p.rapidapi.com")
 
 headers = {
     'accept-language': "he-IL",
     'x-rapidapi-key': "f1454ed893msh2d4e8acf6f46075p1e3f71jsn7fcf994b62f4",
-    'x-rapidapi-host': "quotes-end-of-day.p.rapidapi.com"
+    'x-rapidapi-host': "securities-end-of-day-trading-data.p.rapidapi.com"
     }
 
-conn.request("GET", "/securities/quotes-end-of-day/1159029/2020/10/19?offset=0&limit=50", headers=headers)
+conn.request("GET", "/securities/end-of-day-trading-data/2020/3/16?securityId=1159029", headers=headers)
 
 res = conn.getresponse()
 data = res.read()
